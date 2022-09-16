bcdc39848fc4205c2be2bb10f219bb86c4e4a33b
Racheldeng1995 <racheldeng1995@gmail.com>
2022-09-16 13:42:23 -0700

    update user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const farmRoutes = require('./farm-routes');
 const animalRoutes = require('./animal-routes');
 const transactionRoutes = require('./transaction-routes');
 
-router.use('/user', userRoutes);
+router.use('/users', userRoutes);
 router.use('/farms', farmRoutes);
 router.use('/animals', animalRoutes);
 router.use('/transactions', transactionRoutes);
